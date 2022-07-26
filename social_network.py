126057b749023d92bc4f56e20101da38d4521d4c
waka horie <wakahorie@wakas-MacBook-Pro.local>
2022-07-26 14:57:50 -0400

    finished

--- social_network.py
+++ social_network.py
@@ -1,28 +1,15 @@
 #Various import Statements can go here
-from  social_network_classes import SocialNetwork,Person
+from  social_network_classes import SocialNetwork, Person
 import social_network_ui
 
 
 
-def mainMenu():
-    print("")
-    print("1. Create a new account")
-    print("2. Manage my account")
-    print("3. Quit")
-    print("********************************************************")
-    return input("Please Choose a number: ")
 
-def manageAccountMenu():
-    print("1. Edit my details")
-    print("2. Add a friend")
-    print("3. View all my friends")
-    print("4. View all my messages")
-    print("5. <- Go back ")
-    return input("Please Choose a number: ")
 
 
 #Create instance of main social network object
 ai_social_network = SocialNetwork()
+
 
 #The line below is a python keyword to specify which 
 if __name__ == "__main__":
@@ -33,20 +20,52 @@ if __name__ == "__main__":
     choice = social_network_ui.mainMenu()
 
     while True: 
-        if choice == "1":
+        if choice == 1:
             print("\nYou are now in the create account menu")
             ai_social_network.create_account()
 
-        elif choice == "2":
+        elif choice == 2:
             inner_menu_choice = social_network_ui.manageAccountMenu()
-            #Handle inner menu here
             while True:
-                if inner_menu_choice == "5":
+                current_user = None
+                current_user_name = input("please give current account")
+                for people in ai_social_network.list_of_people:
+                    if people.id == current_user_name:
+                        current_user = people
+                if inner_menu_choice == 1:
+                    print("you can now edit your profile")
+                    current_user.edit_profile()
+
+                elif inner_menu_choice == 2:
+                    friend_choice = input("these are your options to do with friends\n 1. add friend\n 2. remove friend\n :")
+                    if friend_choice == "1":
+                        print("add friends here now")
+                        current_user.add_friend()
+                    elif friend_choice == "2":
+                        print("remove friend of your choice")
+                        current_user.block_friends()
+
+                elif inner_menu_choice == 3:
+                    print("view friends here")
+                    current_user.view_friends()
+
+                elif inner_menu_choice == 4:
+                    message_choice = input("these are your options to do with friends\n 1. send message \n 2. view my messages ")
+                    if message_choice == "1":
+                        print("send a message here")
+                        current_user.send_message()
+                    elif message_choice == "2":
+                        print("view message here")
+                        current_user.view_message()
+        
+
+                
+                if inner_menu_choice == 5:
                     break
                 else:
                     inner_menu_choice = social_network_ui.manageAccountMenu()
 
-        elif choice == "3":
+        elif choice == 3:
             print("Thank you for visiting. Goodbye3")
             break
 
--- social_network_classes.py
+++ social_network_classes.py
@@ -5,57 +5,81 @@ class SocialNetwork:
                                  # you can save objects of people on the network in this list
         
     ## For more challenge try this
-    def save_social_media(self):
-        # function to save social media to a file on disk 
-        # hint: look up how to use python's inbuil json module to turn objects to json
-        # you can write this json unto a file on disk
-        pass
-
-    ## For more challenge try this
-    def reload_social_media(self):
+    #def reload_social_media(self):
         # function to load saved social media from file on disk 
         # hint: load a the json file from disk and look up how to recreate the list of people objects.
-        pass
+        
 
     def create_account(self):
         #implement function that creates account here
-        username = input("please type in your username: ")
+        name = input("please type in your username")
+        while name in self.list_of_people:
+          name = input("please type in another username: ")
+          if name not in self.list_of_people:
+            break
         age = input("please type in age")
-
-        user = Person(username , age)
+        user = Person(name , age)
         self.list_of_people.append(user)
+        print("now our username is set to" , name , "and your age is set to " , age)
 
 
-        #for loop to check name in list_of_people
-        #if username != self.list_of_people:
-            #self.list_of_people.append(username)
-        #     age = input("please type in age")
-        #     #do not know if this is correct #
-            
-        #     self.list_of_age.append(age)
-        #     print("Creating ...")
-        #     print("account created")
-            
-        # else:
-        #     print("username already used")
-        #     print("please try again")
         
-        #how to ask for the username again
-        
-        #implement function that creates account here
-        # pass
-
+       
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self , name , age):
         self.id = name
         self.year = age
         self.friendlist = []
+        self.messagelist = []
 
-    def add_friend(self, person_object):
-        #implement adding friend. Hint add to self.friendlist
-        pass
+    def edit_profile(self):
+        newname = input("please type in your new name")
+        self.id = newname
+        newage = input("please type in your new age")
+        self.age = newage
+        print("this is your new name", newname, " and this is your new age" , newage)
+
+
+     
+
+    def add_friend(self):
+         name = input("please type in the username: ")
+         for i in range(0,len(self.friendlist)):
+            if name != self.friendlist[i]:
+             self.friendlist.append(name)
+             print(name , "added to the friendlist")
+            else:
+             print("username already added to friendlist")
+
+
+    def view_friends(self):
+        print("your current friend list" , self.friendlist)
+
+       
+    
 
     def send_message(self):
+         message = input("type in you message")
+         receiver = input("message receiver username: ")
+         for i in range(0,SocialNetwork.list_of_people):
+            if receiver == SocialNetwork.list_of_people[i]:
+                receiver.self.messagelist.append(message)
+                print("the message: ", message , "   has been sent to:  " , receiver)
+
+    def view_message(self):
+        print("current message list" , self.messagelist)
+
         #implement sending message to friend here
-        pass
+    
+
+    def block_friends(self):
+        notfriend = input("who do you want to block: ")
+        for i in range(0,len(self.freindlist)):
+            if notfriend == self.friendlist[i]:
+                self.friendlist.pop(notfriend)
+                print(notfriend , "has been removed from your friend list")
+            else:
+                print("already not your friend")
+
+   
